1ff06fdf750a7bf0d32a3b04824aa6b3bf884a00
Nicholas Nathan Colotouros <NNColotouros@gmail.com>
2015-11-21 11:40:35 -0500

    Parser Finished

--- src/Parser.py
+++ src/Parser.py
@@ -1,26 +1,15 @@
-# Currently doesn't know how to N/A
-
 import sys
 import re
 
 NUM_COLS = 266
 
 rawInput = sys.stdin.read()
-rawInput = re.sub('\r', ' ', rawInput) # Strip redundant line return
-raw_input_asList = re.split(r'\t+|\n+', rawInput)
+rawInput = re.sub('\r', '', rawInput) # Strip redundant line return
+raw_input_byRow = re.split(r'\n', rawInput)
 
 parsedInput = []
 col = 0
 row = 0
-for entry in raw_input_asList:
-	if col % NUM_COLS == 0:
-		parsedInput.append([])
-		row += 1
-
-	parsedInput[row-1].append(entry)
-	col += 1
-
-# For testing
-for l in parsedInput:
-    print("-----------------NEW ROW ------------------------")
-    print(l)
+for row in raw_input_byRow:
+    raw_input_asList = re.split(r'\t| |,|;', row)
+    parsedInput.append(raw_input_asList)
