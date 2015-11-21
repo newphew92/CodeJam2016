bb42da61c5208036c5d17412f3018e0092776458
Nicholas Nathan Colotouros <NNColotouros@gmail.com>
2015-11-21 11:41:53 -0500

    removed more dead code

--- src/Parser.py
+++ src/Parser.py
@@ -6,8 +6,6 @@ rawInput = re.sub('\r', '', rawInput) # Strip redundant line return
 raw_input_byRow = re.split(r'\n', rawInput)
 
 parsedInput = []
-col = 0
-row = 0
 for row in raw_input_byRow:
     raw_input_asList = re.split(r'\t| |,|;', row)
     parsedInput.append(raw_input_asList)
