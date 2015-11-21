b4e7fb19d3ff17e204a202d680569129dfad76a9
Nicholas Nathan Colotouros <NNColotouros@gmail.com>
2015-11-21 11:51:40 -0500

    Update Parser.py

--- src/Parser.py
+++ src/Parser.py
@@ -1,11 +1,15 @@
 import sys
 import re
 
-rawInput = sys.stdin.read()
-rawInput = re.sub('\r', '', rawInput) # Strip redundant line return
-raw_input_byRow = re.split(r'\n', rawInput)
-
-parsedInput = []
-for row in raw_input_byRow:
-    raw_input_asList = re.split(r'\t| |,|;', row)
-    parsedInput.append(raw_input_asList)
+# Returns a list of lists. Each inner list is a row in the table
+def parseInput():
+    rawInput = sys.stdin.read()
+    rawInput = re.sub('\r', '', rawInput) # Strip redundant line return
+    raw_input_byRow = re.split(r'\n', rawInput)
+    
+    parsedInput = []
+    for row in raw_input_byRow:
+        raw_input_asList = re.split(r'\t| |,|;', row)
+        parsedInput.append(raw_input_asList)
+    
+    return parsedInput
