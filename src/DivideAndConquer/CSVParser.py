61e575e87b46e5e48dcb4ad42188bb256415b6ad
Nicholas Nathan Colotouros <NNColotouros@gmail.com>
2015-11-22 15:17:49 -0500

    Create CSVParser.py

---
+++ src/DivideAndConquer/CSVParser.py
@@ -0,0 +1,16 @@
+# Takes as input the location of the CSV file to be tested and turns it into a proper csv
+import sys
+import re
+
+# Returns a list of lists. Each inner list is a row in the table
+def parseInput():
+    rawInput = sys.stdin.read()
+    rawInput = re.sub('\r', '', rawInput) # Strip redundant line return
+    raw_input_byRow = re.split(r'\n', rawInput)
+    
+    parsedInput = []
+    for row in raw_input_byRow:
+        raw_input_asList = re.split(r'\t| |,|;', row)
+        parsedInput.append(raw_input_asList)
+    
+    return parsedInput
